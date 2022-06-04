12474aa5e075c5f7db7a10c0c96d01fb4d55f077
a2k6 <adrianpanezic@comcast.net>
2022-06-03 21:58:34 -0700

    Bug and efficiency fixes

--- index.js
+++ index.js
@@ -1,24 +1,3 @@
-function isMobile() { // This does its detection based on aspect ratio
-    let windowWidth = document.documentElement.clientWidth;
-    if (windowWidth < 700) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-function swapStyleSheet() { // Change the website stylesheet
-    let documentStyle = document.getElementById("stylesheet");
-    let currentStylesheet = documentStyle.getAttribute("href");
-    if (currentStylesheet === "style.css" && isMobile()) {
-        console.log(isMobile);
-        documentStyle.setAttribute("href", "mobile.css");
-    } else if ((currentStylesheet === "mobile.css") && (!isMobile()) ) {
-        console.log(isMobile);
-        documentStyle.setAttribute('href', "style.css");
-    }
-}
-
 function fitDimensions(oldWidth, oldHeight) { // Fits the image to the screen
     let windowWidth = document.documentElement.clientWidth;
     let windowHeight = document.documentElement.clientHeight;
@@ -84,17 +63,5 @@ function displayImage(pathToImage, name) { // Displays image on screen
 }
 
 function loadImage(pathToImage, name) { // Decides if the browser is viewed on mobile or not
-    if (!isMobile()) {
         displayImage(pathToImage, name);
-    }
-}
-
-// window.onresize = () => { // when the image is resized
-//     console.log(isMobile());
-//     swapStyleSheet();
-// }
-
-window.onload = () => {
-    console.log(isMobile());
-    swapStyleSheet();
 }
